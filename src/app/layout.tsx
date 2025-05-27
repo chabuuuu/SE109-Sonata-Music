f7d40d78f5f9c736c6eea94914c7deaad727778c
Khoahuynh2511 <dangkhoahuynh2511@gmail.com>
2025-05-27 09:33:41 +0700

    Fix music page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { MusicPlayerProvider } from "@/context/MusicPlayerContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {children}
+          <MusicPlayerProvider>
+            {children}
+          </MusicPlayerProvider>
         </AuthProvider>
       </body>
     </html>
